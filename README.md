4d5150ba097761719cb3d4da52e2aa07071eea4a
LeoKuhorev <kuhorev.leonid@gmail.com>
2019-08-15 14:54:06 -0700

    deleted functions, added new array for notification messages, added loop for questions 1-6

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 201-lab02
 
+Thanks to [www.w3schools.com](https://www.w3schools.com/js/js_random.asp) for the js random tutorial
+
+
 ![Google accessibility tool](images/20190813.jpg)
--- js/app.js
+++ js/app.js
@@ -1,50 +1,13 @@
 'use strict';
 
-//declaring global variables
-var answerStatus; //correct or incorrect answer
-var correctAnswersCount; //stores number of correct answers
-// var timer; //timer for the guessing game
-
-//array with questions
-var questions = ['I spent 5 years studying tribology', 'My parked car was almost destroyed by an old lady a few weeks ago', 'I have a collection of 50 rare butterflies', 'I used to have 8 cats', 'I climbed Elbrus last year', 'I spent 3 nights in Mojave Desert in Nevada because my car broke down', 'I jumped with parachute', 'I hate dust so I vacuum every day no matter what'];
-
-//array with answers
-var correctAnswers = ['yes', 'yes', 'no', 'yes', 'no', 'no', 'yes', 'no'];
-
-// create a function that allows only yes or no answers and replaces y/n with yes/no
-var userInput = function (question) {
-  var answer = prompt(question);
-  while (!(answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'no' || answer.toLowerCase() === 'y' || answer.toLowerCase() === 'n')) {
-    console.log('Unacceptable user entry - "' + answer + '", user prompted to try again');
-    answer = prompt('I\'m sorry, I didn\'t quite get your answer, can you please try again using only yes and no?  ' + question);
-  }
-  if(answer.toLowerCase() === 'y') {
-    answer = 'yes';
-  } else if (answer.toLowerCase() === 'n') {
-    answer = 'no';
-  }
-  return answer.toLowerCase();
-};
-
-//create a function that compares user answer with correct answer
-var answerVerification = function (question, answer){
-  if(question === answer) {
-    alert('Yay! You\'re absolutely right!');
-    answerStatus = 'correct answer';
-    correctAnswersCount++;
-  } else {
-    alert('Oh, unfortunately you\'re not right, hope you\'ll be more lucky next time!');
-    answerStatus = 'incorrect answer';
-  }
-};
-
+//***CHANGING USER NAME ***
 //change user name by clicking change name button, otherwise show as guest
 var userName = 'guest'; //default user name
 var userNameEl = document.getElementById('user-name'); //right top corner
 var userNameEl2 = document.getElementById('user-name2'); // #about p
 var changeNameEl = document.getElementById('add-name'); //change name button
 
-var changeUserName = function () {
+var changeUserName = function() {
   userName = prompt('Welcome to my page! What\'s your name?');
   if (userName === null || userName === '') {
     userName = 'guest';
@@ -52,26 +15,84 @@ var changeUserName = function () {
   userNameEl.textContent = 'Welcome ' + userName + '!';
   userNameEl.style.padding = '14px 16px';
   userNameEl2.textContent = userName;
-  console.log('the username is ' + userName);
+  console.log('the username entered through the form: ' + userName);
 };
 changeNameEl.addEventListener('click', changeUserName);
 
-//create a function that starts guessing game when play button is clicked
+//***GUESSING GAME***
+var correctAnswersCount; //stores number of correct answers
+
+//questions
+var questions = ['I have a collection of 50 rare butterflies', 'I used to have 8 cats', 'I climbed Elbrus last year', 'I spent 3 nights in Mojave Desert in Nevada because my car broke down', 'I jumped with parachute', 'How many marshmallows I can fit in my mouth?', 'Try to guess any country I\'ve ever been to'];
+
+//answers
+var marshmallowsRandom = Math.floor(Math.random() * 20) + 10; //assigns random value between 10 and 30, thanks to w3schools.com;
+var countriesVisited = ['Belarus', 'France', 'Canada', 'Germany', 'Poland', 'Lithuania', 'Latvia', 'Russia', 'Netherlands', 'Belgium', 'Moldova', 'Romania', 'Bulgaria', 'USA']; //stores visited countries
+var correctAnswers = ['no', 'yes', 'no', 'no', 'yes', marshmallowsRandom, countriesVisited];
+
+//notification messages
+var notificationMessage = ['I don\'t know anything about butterflies', 'I used to be a volunteer and take care of stray animals, so sometimes we would have a lot of them at our house', 'I haven\'t done it just yet, but it\'s on my bucket list', 'I actually never been to Mojave Desert', 'I did it, and it was really cool!', 'Honestly, I\'ve never tried to count and the answer is just a random number, but good guess anyways!'];
+
+//create a that starts guessing game when play button is clicked
 var playButtonEl = document.getElementById('play-game');
 var scoreEl = document.getElementById('score');
 
 //guessing game function
-var guessingGame = function () {
-  // timer = 0;
-  // console.log('entering function timer = ' + timer);
-  // timer = window.setInterval(function(){timer++;console.log('inside setInterval function timer = ' + timer);}, 1000); //something is wrong here - return value of 26
-  alert('Welcome to the guessing game, ' + userName + '. In this game I\'ll tell you a few facts about me, try to guess whether they are true or not. Please use only YES or NO to answer the questions');
+var guessingGame = function() {
+  //if user name is not entered - prompt to enter it
+  if (userName === 'guest') {
+    userName = prompt('Welcome to the guessing game, ' + userName + '. In this game I\'ll tell you a few facts about me, try to guess whether they are true or not. Please use only YES or NO to answer the first 5 questions. But first, please tell me your name ');
+    console.log('the username entered at the game start: ' + userName);
+    while (userName === null || userName === '' || userName ==='guest') {
+      userName = prompt('I really think you should tell me who you are :-)');
+    }
+    //change user name on the site
+    userNameEl.textContent = 'Welcome ' + userName + '!';
+    userNameEl.style.padding = '14px 16px';
+    userNameEl2.textContent = userName;
+  } else {
+    alert('Welcome to the guessing game, ' + userName + '. In this game I\'ll tell you a few facts about me, try to guess whether they are true or not. Please use only YES or NO to answer the first 5 questions');
+  }
   correctAnswersCount = 0;
   for (var i = 0; i < questions.length; i++) {
-    var question = userInput(questions[i]);
-    answerVerification(question, correctAnswers[i]);
-    console.log('Question ' + (i + 1) +': "' + questions[i] + '". User answer: ' + question + ' - ' + answerStatus + '. Correct answers: '+ correctAnswersCount + ' out of ' + questions.length);
-    // console.log('inside for loop timer = ' + timer);
+    var answer = prompt(questions[i]).toLowerCase();
+    //for the first 5 questions allow only yes/y or no/n answers and convert y/n to yes/no
+    if (i < questions.length-2){
+      while (!(answer === 'yes' || answer === 'no' || answer === 'y' || answer === 'n')) {
+        answer = prompt(' I\'m sorry, I didn\'t quite get your answer, please try again using only yes and no answers. ' + questions[i]).toLowerCase();
+      }
+      if (answer === 'y') {
+        answer = 'yes';
+      } else if (answer === 'n') {
+        answer = 'no';
+      }
+    }
+    //for the 6th question allow only numeric answers
+    if (i === questions.length-2) {
+      console.log('the random generated number of marshmallows is - ' + correctAnswers[i]);
+      for (var k = 0; k < 3; k++) {
+        while (isNaN(answer) || answer === null || answer === '') {
+          answer = prompt('Please enter a number for the answer');
+        }
+        if (answer < 5) {
+          answer = prompt('Wow, ' + userName + ', you really underestimate me! Try again, you have ' + (3-k) + ' attempts left');
+        } else if (answer > 50) {
+          answer = prompt('Wow, ' + userName + ', you really overestimate me! Try again you have ' + (3-k) + ' attempts left');
+        } else if (answer < correctAnswers[i]) {
+          answer = prompt('Pretty close, ' + userName + ', but try a bit higher, you have ' + (3-k) + ' attempts left');
+        } else if (answer > correctAnswers[i]) {
+          answer = prompt('Pretty close, ' + userName + ', but try a bit lower, you have ' + (3-k) + ' attempts left');
+        }
+      }
+      answer = parseInt(answer);
+    }
+    if (answer === correctAnswers[i]) {
+      alert('Yay ' + userName + ' you\'re absolutely right! ' + notificationMessage[i]);
+      correctAnswersCount++;
+    } else {
+      alert('Nope, ' + userName + ' not really, ' + notificationMessage[i]);
+    }
+    console.log('Question ' + (i + 1) +': "' + questions[i] + '". User answer: ' + answer + ' . Correct answers: '+ correctAnswersCount + ' out of ' + questions.length);
   }
   //notifying user about the game score
   if(correctAnswersCount > 5) {
@@ -83,7 +104,5 @@ var guessingGame = function () {
   }
   playButtonEl.textContent = 'PLAY AGAIN';
   scoreEl.innerHTML = '<br> Your last result is: '+ correctAnswersCount + ' correct answers out of ' + questions.length + ' questions. Do you want to play again?  ';
-  // console.log('exiting function timer = ' + timer);
-  // window.clearInterval(timer);
 };
 playButtonEl.addEventListener('click', guessingGame);
