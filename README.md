e62e04d0cb833d2a02b66f58d1fd95bd474b0536
LeoKuhorev <kuhorev.leonid@gmail.com>
2019-08-16 12:05:34 -0700

    added comments to .js, added description to .md, fixed bug with double console.log message

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # 201-lab02
 ## Welcome to my Code Fellows 201 Lab 02 project
 
-###Short features description:
+### Short features description:
 - you can enter user name in the top right corner (unfortunately it won't be saved after you refresh the page);
 - if you want to find out more facts about me you can play a funny question game. To do that just click on "play" button;
-- the questions will be shown in random order;
+- questions, correct answers and messages are stored in 3 different arrays, the questions will be shown in random order. The program checks the type of the answer and starts corresponding lines of code; 
+- if new questions are added to the array the random iteration and indexes will work just fine, some of the messages still need to be adjusted, though, so that they show only relevant to the question info);
 
 ### Here's a screenshot of Google Audits tool
 ![Google accessibility tool](images/20190813.jpg)
--- js/app.js
+++ js/app.js
@@ -20,8 +20,6 @@ var changeUserName = function() {
 changeNameEl.addEventListener('click', changeUserName);
 
 //***GUESSING GAME***
-var correctAnswersCount; //stores number of correct answers
-
 //questions
 var questions = [
   'I have a collection of 50 rare butterflies',
@@ -55,52 +53,60 @@ var notificationMessage = [
   'Placeholder'
 ];
 
-//create a that starts guessing game when play button is clicked
+//create a function that starts guessing game when play button is clicked
 var playButtonEl = document.getElementById('play-game');
 var scoreEl = document.getElementById('score');
 
 //guessing game function
 var guessingGame = function() {
-  //if user name is not entered - prompt to enter it
+
+  //if user name is not entered before - prompt to enter it
   if (userName === 'guest') {
     userName = prompt('Welcome to the guessing game, ' + userName + '. In this game I\'ll tell you a few facts about me, try to guess whether they are true or not. But first, please tell me your name ');
     console.log('the username entered at the game start: ' + userName);
     while (userName === null || userName === '' || userName ==='guest') {
       userName = prompt('I really think you should tell me who you are :-)');
     }
-    //change user name in the site html
+
+    //change user name in the site html and remove change name button
     userNameEl.textContent = 'Welcome ' + userName + '!';
     userNameEl.style.padding = '14px 16px';
     userNameEl2.textContent = userName;
+
+    //if name is entered before - just start the game
   } else {
     alert('Welcome to the guessing game, ' + userName + '. In this game I\'ll tell you a few facts about me, try to guess whether they are true or not.');
   }
 
-  correctAnswersCount = 0; //start the game with 0 correct answers
+  //always start the game with 0 correct answers
+  var correctAnswersCount = 0;
 
-  //iterate through the array of questions in random order (thanks to https://www.kirupa.com/html5/shuffling_array_js.htm)
+  //iterate through the arrays of questions/answers in random order (thanks to https://www.kirupa.com/html5/shuffling_array_js.htm)
   for (var i = questions.length-1; i >=0; i--) {
-    var randomIndex = Math.floor(Math.random()*(i+1)); //stores the random number between 0 and current index
 
-    //storing random array value
-    var itemAtIndexQuestions = questions[randomIndex]; //stores random array value for questions array
-    var itemAtIndexCorrectAnswers = correctAnswers[randomIndex]; //stores random array value for correctAnswers array
-    var itemAtIndexNotificationMessage = notificationMessage[randomIndex]; //stores random array value for notificationMessage array
+    //store random number between 0 and current index
+    var randomIndex = Math.floor(Math.random()*(i+1));
 
-    //swapping values of current index and random index
-    //random array element gets current i element value
+    //store random array value in a variable for each array
+    var itemAtIndexQuestions = questions[randomIndex]; //for questions array
+    var itemAtIndexCorrectAnswers = correctAnswers[randomIndex]; //for correctAnswers array
+    var itemAtIndexNotificationMessage = notificationMessage[randomIndex]; //for notificationMessage array
+
+    //swapping values of current index element and random index element
+    //random index array element gets current index element value
     questions[randomIndex] = questions[i];
     correctAnswers[randomIndex] = correctAnswers[i];
     notificationMessage[randomIndex] = notificationMessage[i];
 
-    //current i element gets random array element value
+    //current index array element gets random index element value
     questions[i] = itemAtIndexQuestions;
     correctAnswers[i] = itemAtIndexCorrectAnswers;
     notificationMessage[i] = itemAtIndexNotificationMessage;
 
+    //prompt questions from the array, bring them lo lowercase
     var answer = prompt(questions[i]).toLowerCase();
 
-    //if the answer is text, then allow only yes/y or no/n answers and convert y/n to yes/no
+    //if the answer is a string, allow user only yes/y or no/n answers and convert y/n to yes/no
     if (typeof correctAnswers[i] === 'string'){
       while (!(answer === 'yes' || answer === 'no' || answer === 'y' || answer === 'n')) {
         answer = prompt(' I\'m sorry, I didn\'t quite get your answer, please try again using only yes and no answers. ' + questions[i]).toLowerCase();
@@ -111,7 +117,7 @@ var guessingGame = function() {
         answer = 'no';
       }
 
-      //if the answer is a number allow user only numeric entries and give 4 attempts to guess the number
+    //if the answer is a number, allow user only numeric entries and give 4 attempts to guess the number
     } else if (!isNaN(correctAnswers[i])) {
       for (var k = 0; k < 3; k++) {
         while (isNaN(answer) || answer === null || answer === '') {
@@ -128,12 +134,15 @@ var guessingGame = function() {
           answer = prompt('Pretty close, ' + userName + ', but try a bit higher, you have ' + (3-k) + ' attempts left');
         } else if (answer > correctAnswers[i]) {
           answer = prompt('Pretty close, ' + userName + ', but try a bit lower, you have ' + (3-k) + ' attempts left');
+        } else {
+          break;
         }
       }
-      //convert prompt string into number to be able to use '==='
+
+      //convert user entry into number to be able to use '===' rather than '=='
       answer = parseInt(answer);
 
-    //if the answer is an array allow user only text entries and give 6 attempts to guess the answer
+    //if the answer is an array allow user only text entries, match the entry with array elements, and give 6 attempts to guess the answer
     } else if (Array.isArray(correctAnswers[i])) {
       var correctCountry;
       for (var n = 0; n < 5; n++) {
@@ -148,7 +157,8 @@ var guessingGame = function() {
             break;
           }
         }
-        //if user guessed the country - exit the loop
+
+        //if user entry matches any array element form the array - exit the loop and show alert with list of all array elements
         if (matchesFound > 0) {
           alert('Yay ' + userName + ', you\'re absolutely right! I\'ve been to ' + correctCountry + ' indeed. Here\'s the list of countries I visited: ' + correctAnswers[i].join(', '));
           correctAnswersCount++;
@@ -156,15 +166,17 @@ var guessingGame = function() {
           break;
         } else {
           console.log('List of countries I\'ve been to: ' + correctAnswers[i].join(', ') + '. User answer is - ' + answer);
-          answer = prompt('No, I haven\'t been there yet, try again, you have ' + (5-n) + ' attempts left');
+          answer = prompt('No, I haven\'t been to ' + answer + ' yet, try again, you have ' + (5-n) + ' attempts left');
         }
       }
-      //if after 6 attempts no country guessed - show alert with a list of countries
+
+      //if after 6 attempts no matches found - show alert with a list of array elements user tried to guess
       if (matchesFound === 0) {
         alert('Ooops, you\'re out of attempts, don\'t worry, you\'ll be more lucky next time! By the way, here\'s the list of countries I visited: ' + correctAnswers[i].join(', '));
       }
     }
-    //checking if answers are correct for question 1-6
+
+    //checking if answers are correct for string and numeric types of answers
     if (!Array.isArray(correctAnswers[i])) {
       if (answer === correctAnswers[i]) {
         alert('Yay ' + userName + ', you\'re absolutely right! ' + notificationMessage[i]);
@@ -175,10 +187,11 @@ var guessingGame = function() {
       console.log('Question ' + (correctAnswers.length - i) +': "' + questions[i] + '". User answer: ' + answer + ' . Correct answers: '+ correctAnswersCount + ' out of ' + questions.length);
     }
   }
+
   //notifying user about the game score
-  if(correctAnswersCount > 5) {
+  if(correctAnswersCount > 0.8*questions.length) {
     alert('Congratulations ' + userName + '! You\'ve got ' + correctAnswersCount + ' out of ' + questions.length + ' correct answers. Seems that you know me pretty well!');
-  } else if (correctAnswersCount > 3) {
+  } else if (correctAnswersCount > 0.5*questions.length) {
     alert('Pretty close, ' + userName + '! You\'ve got ' + correctAnswersCount + ' out of ' + questions.length + ' correct answers, you can try again to get ' + questions.length + '/' + questions.length + '!');
   } else {
     alert('You\'ve got ' + correctAnswersCount + ' out of ' + questions.length + ' correct answers. Cheer up, ' + userName + '! I\'m sure you can get more next time! :-)');
